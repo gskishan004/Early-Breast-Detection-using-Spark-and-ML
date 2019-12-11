9d6d387d2cd6fec2331556cb47928dee6929174b
gskishan004 <gskishan004@users.noreply.github.com>
2019-12-10 23:52:36 -0500

    stable version

--- breast_cancer_eval.py
+++ breast_cancer_eval.py
@@ -2,18 +2,42 @@ from pyspark.ml.classification import LogisticRegressionModel
 from pyspark.ml import Pipeline, PipelineModel
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
+import sparkdl as dl
+import tensorflow as tf
+import pyspark.sql.functions as f
+import sparkdl as dl
+from pyspark.sql.functions import lit
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
 
-lr_test = LogisticRegressionModel.load('./test-1')
+img_dir = "filtered_dataset/"
+magnifications = ['40','100', '200', '400']
 
-# Use a featurizer to use trained features from an existing model
-featurizer_test = dl.DeepImageFeaturizer(inputCol = "image", outputCol = "features", modelName = "InceptionV3")
+for m in magnifications:
 
-# Setup a pipeline
-p_lr_test = PipelineModel(stages=[featurizer_test, lr_test])
+	b_df = dl.readImages(img_dir + "/b" + m).withColumn("label", lit(1))
+	m_df = dl.readImages(img_dir + "/m" + m).withColumn("label", lit(0))
 
-# Test and evaluate
-tested_lr_test = p_lr_test.transform(testDF)
-evaluator_lr_test = MulticlassClassificationEvaluator(metricName = "accuracy")
-print("Logistic Regression Model: Test set accuracy = " + str(evaluator_lr_test.evaluate(tested_lr_test.select("prediction", "label"))))
 
-tested_lr_test.select("label", "probability", "prediction").show(20, False)
+	#Splitting the data into training and test in the ratio 80% & 20%
+	trainb, testb = b_df.randomSplit([80.00, 20.00], seed=42)
+	trainm, testm = m_df.randomSplit([80.00, 20.00], seed=42)
+
+	#combining the dataset benign and malignanent for the training and testing
+	trainDF = trainb.unionAll(trainm)
+	testDF = testb.unionAll(testm)
+
+	lr_test = LogisticRegressionModel.load('./test-'+m)
+
+	# Use a featurizer to use trained features from an existing model
+	featurizer_test = dl.DeepImageFeaturizer(inputCol = "image", outputCol = "features", modelName = "InceptionV3")
+
+	# Setup a pipeline
+	p_lr_test = PipelineModel(stages=[featurizer_test, lr_test])
+
+	# Test and evaluate
+	tested_lr_test = p_lr_test.transform(testDF)
+	evaluator_lr_test = MulticlassClassificationEvaluator(metricName = "accuracy")
+	print("Logistic Regression Model: Test set accuracy = " + str(evaluator_lr_test.evaluate(tested_lr_test.select("prediction", "label"))))
+
+	tested_lr_test.select("label", "probability", "prediction").show(20, False)
--- breast_cancer_train.py
+++ breast_cancer_train.py
@@ -2,36 +2,36 @@ import tensorflow as tf
 import pyspark.sql.functions as f
 import sparkdl as dl
 from pyspark.sql.functions import lit
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
 
 #root directory
 img_dir = "filtered_dataset/"
 
-#currently testing accuracy for 40X magnigication for both malignant and benign 
+magnifications = ['40','100','200', '400']
+
+for m in magnifications:
+	b_df = dl.readImages(img_dir + "/b" + m).withColumn("label", lit(1))
+	m_df = dl.readImages(img_dir + "/m" + m).withColumn("label", lit(0))
 
 
-b_df = dl.readImages(img_dir + "/b40").withColumn("label", lit(1))
-m_df = dl.readImages(img_dir + "/m40").withColumn("label", lit(0))
+	#Splitting the data into training and test in the ratio 80% & 20%
+	trainb, testb = b_df.randomSplit([80.00, 20.00], seed=42)
+	trainm, testm = m_df.randomSplit([80.00, 20.00], seed=42)
+
+	#combining the dataset benign and malignanent for the training and testing
+	trainDF = trainb.unionAll(trainm)
+	testDF = testb.unionAll(testm)
 
 
-#Splitting the data into training and test in the ratio 80% & 20%
-trainb, testb = b_df.randomSplit([80.00, 20.00], seed=42)
-trainm, testm = m_df.randomSplit([80.00, 20.00], seed=42)
+	vectorizer = dl.DeepImageFeaturizer(inputCol="image", outputCol="features", modelName='InceptionV3')
+	logreg = LogisticRegression(maxIter=10,regParam=0.01, elasticNetParam=0.1, labelCol = "label", featuresCol="features")
+	pipeline = Pipeline(stages=[vectorizer, logreg])
 
-#combining the dataset benign and malignanent for the training and testing
-trainDF = trainb.unionAll(trainm)
-testDF = testb.unionAll(testm)
-
-#using Logistic Regression model for training 
-from pyspark.ml.classification import LogisticRegression
-from pyspark.ml import Pipeline
-vectorizer = dl.DeepImageFeaturizer(inputCol="image", outputCol="features", modelName='InceptionV3')
-logreg = LogisticRegression(maxIter=10,regParam=0.01, elasticNetParam=0.1, labelCol = "label", featuresCol="features")
-pipeline = Pipeline(stages=[vectorizer, logreg])
-
-pipeline_model = pipeline.fit(trainDF)
+	pipeline_model = pipeline.fit(trainDF)
 
 
-lrModel = pipeline_model
-print(lrModel)
+	lrModel = pipeline_model
+	print(lrModel)
 
-lrModel.stages[1].write().overwrite().save('test-1')
+	lrModel.stages[1].write().overwrite().save('test-'+m)
